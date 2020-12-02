d3f2a8f4a2cdf6e80d010ac2700667532a8b1786
st-laptop <sebastian.taras12@gmail.com>
2020-12-02 14:06:55 +0100

    .count

--- day02/02.py
+++ day02/02.py
@@ -1,10 +1,3 @@
-def num_occurences(char, password):
-    occ = 0
-    for c in password:
-        if c == char:
-            occ += 1
-    return occ
-
 def is_password_valid(char, index, password):
     return True if password[index-1] == char else False
 
@@ -13,12 +6,12 @@ f = open("02input.txt")
 passwords = list(map(lambda x: x.strip().split(": "), f.readlines()))
 num_valid_policy1 = 0
 num_valid_policy2 = 0
-for p in passwords:
+for p in passwords:     #p[0] = rules, p[1] = password
     lower = int(p[0].split("-")[0])
     upper = int(p[0].split("-")[1].split(" ")[0])
     char = p[0].split(" ")[1]
     
-    if num_occurences(char, p[1]) >= lower and num_occurences(char, p[1]) <= upper:
+    if p[1].count(char) >= lower and p[1].count(char) <= upper:
         num_valid_policy1 += 1
     
     if is_password_valid(char, lower, p[1]) == True and is_password_valid(char, upper, p[1]) == False:
@@ -26,4 +19,4 @@ for p in passwords:
     elif is_password_valid(char, lower, p[1]) == False and is_password_valid(char, upper, p[1]) == True:
         num_valid_policy2 += 1
 
-print(num_valid_policy1, num_valid_policy2)
+print(num_valid_policy1, num_valid_policy2)S
