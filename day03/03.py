3eb226d466b96886a982aafd0b4e2227a81d0ef3
st-desktop <sebastian.taras12@gmail.com>
2020-12-03 08:18:37 +0100

    refactor day 3

--- day03/03.py
+++ day03/03.py
@@ -1,3 +1,4 @@
+import math
 class Rule:
     def __init__(self, x, y):
         self.x = x
@@ -6,20 +7,23 @@ class Rule:
 def is_tree(c):
     return c == '#'
 
-def extend(line, length):
-    res = line
-    while(len(res) < length):
-        res += res
-    return res
-
-def traverse_map(grid, rule):
+def traverse_map(trees, rule):
     position = 0
     num_trees = 0
+    grid = extend(trees, rule)
     for i in range(0, len(grid)):
         if position < len(grid[i]) and i%rule.y == 0:
-            num_trees = num_trees + 1 if is_tree(map_trees[i][position]) else num_trees
+            num_trees = num_trees + 1 if is_tree(grid[i][position]) else num_trees
             position += rule.x
     return num_trees
+
+def extend(trees, rule):
+    res = list()
+    print(len(trees))
+    for t in trees:
+        res.append(t*math.ceil(((len(trees)*rule.x)/(len(trees[0]))*rule.y)))
+    print(len(res[0]))
+    return res
 
 def product(list):
     res = 1
@@ -30,12 +34,8 @@ def product(list):
 f = open("03input.txt")
 lines = list(map(lambda x: x.strip(), f.readlines()))
 
-map_trees = list()
-for line in lines:
-    map_trees.append(line*100)
-
 # 1.
-print(traverse_map(map_trees, Rule(3, 1)))
+print(traverse_map(lines, Rule(3, 1)))
 
 # 2.
 rules = list()
@@ -47,7 +47,7 @@ rules.append(Rule(1, 2))
 
 results = list()
 for rule in rules:
-    results.append(traverse_map(map_trees, rule))
+    results.append(traverse_map(lines, rule))
 
 #153 66 79 92 33
 print(product(results))
